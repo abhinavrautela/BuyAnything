4ec49ae56e2aec66031b787e1aece4da98489204
abhinav rautela <rautela014@gmail.com>
2022-09-20 12:07:11 +0530

    fix navbar for sm xs screens

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -39,35 +39,47 @@ function Navbar({ totalItems }) {
           </div>
         </Link>
       </div>
-      <div className=" lg:hidden flex flex-col  items-end">
-        <div className="relative">
-          <button
-            className="outline-none"
-            onClick={() => toggalNavbar(!navbar)}
-          >
-            <CgMenuRight size={26} />
-          </button>
-        </div>
+      <div className="lg:hidden flex items-center space-x-3">
+        <Link to="/cart">
+          <div className="flex justify-end ">
+            <FiShoppingCart size={20} />
+            <div className="absolute">
+              <h4 className="text-xs w-4 h-4 flex items-center justify-center font-bold text-white  rounded-full bg-primary -m-2 hover:ring-2 hover:ring-primary hover:bg-white hover:text-primary cursor-pointer">
+                {totalItems}
+              </h4>
+            </div>
+          </div>
+        </Link>
+        <div className=" flex flex-col  items-end">
+          <div className="relative mt-2">
+            <button
+              className="outline-none"
+              onClick={() => toggalNavbar(!navbar)}
+            >
+              <CgMenuRight size={26} />
+            </button>
+          </div>
 
-        <div
-          className = {
-            navbar
-              ? "flex flex-col items-center bg-gray-300 text-xs font-poppins absolute mt-6 duration-200 space-y-1 rounded-md py-3 px-4 shadow-lg"
-              : "right-[-100%] top-0 ease-out duration-300 absolute"
-          }
-        >
-          <Link to="/" className="hover:text-primary ">
-            Home
-          </Link>
-          <Link to="/aboutus" className="hover:text-primary">
-            AboutUS
-          </Link>
-          <Link to="/loginpage" className="hover:text-primary">
-            LogIN
-          </Link>
-          <Link to="/signuppage" className="hover:text-primary">
-            SignUp
-          </Link>
+          <div
+            className={
+              navbar
+                ? "flex flex-col items-center bg-gray-300 text-xs font-poppins absolute mt-9 duration-200 space-y-1 rounded-md py-3 px-4 shadow-lg"
+                : "right-[-100%] top-0 ease-out duration-300 absolute"
+            }
+          >
+            <Link to="/" className="hover:text-primary ">
+              Home
+            </Link>
+            <Link to="/aboutus" className="hover:text-primary">
+              AboutUS
+            </Link>
+            <Link to="/loginpage" className="hover:text-primary">
+              LogIN
+            </Link>
+            <Link to="/signuppage" className="hover:text-primary">
+              SignUp
+            </Link>
+          </div>
         </div>
       </div>
     </div>
