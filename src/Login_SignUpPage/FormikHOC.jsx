348ad8d87a481cd126ef6d1b27920b24553a71ca
abhinav rautela <rautela014@gmail.com>
2022-09-23 23:00:13 +0530

    removed extra code from FormikHOC

--- src/Login_SignUpPage/FormikHOC.jsx
+++ src/Login_SignUpPage/FormikHOC.jsx
@@ -4,11 +4,6 @@ import React from "react";
 const FormikHOC = (Input) => {
   const FormikCostomizedInput = ({ name, ...rest }) => {
     const [{ ...data }, { error, touched }] = useField(name);
-    let borderClass = "border-gray-500";
-
-    if (error && touched) {
-      borderClass = "border-red-500";
-    }
     return <Input {...rest} {...data} error={error} touched={touched} />;
   };
   return FormikCostomizedInput;
