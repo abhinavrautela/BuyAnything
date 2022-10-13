57fc5af98b15fe8674d885a70c739e3edde2ae5f
abhinav rautela <rautela014@gmail.com>
2022-10-13 15:58:23 +0530

    fix bug

--- src/Login_SignUpPage/FormikHOC.jsx
+++ src/Login_SignUpPage/FormikHOC.jsx
@@ -1,10 +1,11 @@
 import { useField } from "formik";
 import React from "react";
 
-const FormikHOC = (Input) => {
-  return ({ name, ...rest }) => {
+const FormikHOC =
+  (Component) =>
+  ({ name, ...rest }) => {
     const [{ ...data }, { ...meta }] = useField(name);
-    return <Input {...rest} {...data} {...meta} />;
+    return <Component {...rest} {...data} {...meta} />;
   };
-};
+
 export default FormikHOC;
